122ecf76ef529109a0a87f6f91fffa992155d071
Sviatoslav Godyk <godyk1992@gmail.com>
2024-11-09 21:24:53 +0800

    task 1

---
+++ js/lesson-1.js
@@ -0,0 +1,22 @@
+// const number = 10;
+// console.log(number);
+// // ! Warning !
+// // ? Question
+// // * Alright
+// // Todo
+// const name = "Andrii";
+// console.log(`Hello ${name}`);
+
+//* Розгалуження: if, if...else, else...if
+//TODO: 1 ===================================
+//Даний рядок, що складається із символів, наприклад, 'abcde'.
+// Перевірте, що першим символом цього рядка є буква 'a'.
+// Якщо це так - виведіть 'так', інакше виведіть 'ні'.
+
+// const str = "abcde";
+// // if (str.startsWith("a")) {
+// //   console.log("yes");
+// // } else {
+// //   console.log("no");
+// // }
+// str[0] === "a" ? console.log("yes") : console.log("no");
