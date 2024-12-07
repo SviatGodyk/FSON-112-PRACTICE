56000359a31425ae332c157cc54d247c1bdc620c
Sviatoslav Godyk <godyk1992@gmail.com>
2024-12-07 21:34:36 +0800

    task-3,4

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -32,3 +32,35 @@
 // const numbers = (param) => param.flatMap((el) => el.values);
 
 // console.log(numbers(data));
+
+//TODO:=========task-03=================
+// Дано масив об'єктів {name: "John", age: 27}, {name: "Jane", age: 31}, {name: "Bob", age: 19}. Перевірте, чи є хоча б один об'єкт з віком менше 20 років. Очікуваний результат: true.
+
+// const people = [
+//   { name: "John", age: 27 },
+//   { name: "Jane", age: 31 },
+//   { name: "Bob", age: 19 },
+// ];
+// const people1 = [
+//   { name: "John", age: 27 },
+//   { name: "Jane", age: 31 },
+//   { name: "Bob", age: 22 },
+// ];
+// const checkAge = (arr) => arr.some(item => item.age < 20);
+// console.log(checkAge(people));
+// console.log(checkAge(people1));
+// const getData = (arr) => {
+//   return arr.map((el, idx) => {
+//     console.log(`${idx + 1} - ${el.name}`);
+//   });
+// };
+// getData(people);
+
+//TODO:=========task-04=================
+//  Дано масив чисел [2, 4, 6, 8, 10]. Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
+
+// const numbers = [2, 4, 6, 8, 10];
+// const numbers1 = [2, 4, 5, 8, 10];
+// const checkEvenNumbersArray = (arr) => arr.every((el) => el % 2 === 0);
+// console.log(checkEvenNumbersArray(numbers));
+// console.log(checkEvenNumbersArray(numbers1));
